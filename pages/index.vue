c7e0408772f6844cc727ab0d260b746e3886fcf8
hrithwikbharadwaj <hrithwik009@gmail.com>
2021-02-19 13:19:14 +0530

    Added Clipboard Functionality

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,22 @@
 <template>
 
-  
+  <!-- Content for Logged in users  -->
   <div v-if="$auth.loggedIn">
-    <!-- TITLE -->
+    
     <section class="section " >
+      <div class="content">
     <h2 class="subtitle home-hero"> Hello {{loggedInUser.name}} </h2>
+      </div>
  
       
     </section>
+<div class="column is-half">
+    <!-- FORM FOR SHORTENING START -->
+    
 
-    <!-- FORM FOR SHORTENING -->
-
+   
     <section>
-       {{shorturl}}
+      <!-- Notifications Start -->
         <b-notification
             v-if="error"
             type="is-danger"
@@ -20,17 +24,34 @@
             role="alert">
            {{error}}
      </b-notification> 
+     <b-notification
+            v-if="copiedText"
+            type="is-success"
+            aria-close-label="Close notification"
+            role="alert">
+           {{copiedText}}
+    </b-notification>
 
+<!-- Notification Section END -->
 
           <b-field label="URL" maxlength="50">
-            <b-input required v-model="urlData.longURL"></b-input>
+            <b-input required
+             v-model="urlData.longURL"
+              icon-right="clipboard"
+              icon-right-clickable
+              @icon-right-click="copyItem"></b-input>
         </b-field>
 
          <b-field label="Slug(optional)">
             <b-input  v-model="urlData.slug"></b-input>
         </b-field>
         <b-button @click="shortenURL(urlData)" type="is-primary is-light">Shorten</b-button>
+        
+         
+        
     </section>
+    
+</div>
   </div>
   
   <div v-else>
@@ -59,6 +80,8 @@ export default {
   data(){
     return{
       error:null,
+      copiedText:"",
+      
       urlData:{
 
         longURL:"",
@@ -74,16 +97,56 @@ export default {
     Card
   },
   methods:{
+    async copyItem(){
+      
+    
+      
+        if (!navigator.clipboard) {
+          // Clipboard API not available
+          return
+        }
+        try {
+          const text = await navigator.clipboard.readText();
+          this.urlData.longURL = text
+          
+        } catch (err) {
+          console.error('Failed to copy!', err)
+       
+     }
+    },
+  //  async  pasteItem(copiedUrl){
+  //     console.log("working copyurl");
+  //           if (!navigator.clipboard) {
+  //         // Clipboard API not available
+  //         return
+  //       }
+  //       const text = copiedUrl
+  //       try {
+  //         await navigator.clipboard.writeText(text)
+  //         this.copiedText = `Copied ${copiedUrl} to clipboard`;
+  //       } catch (err) {
+  //         console.error('Failed to copy!', err)
+  //       }
+  //   },
+      
     async shortenURL(urlData){
      try{
       let res=  await  this.$axios.post('url/shorten',urlData)
-      console.log(res.data.shortUrl)
+      
      this.shorturl= res.data.shortUrl
+     const text = this.shorturl
+        try {
+          await navigator.clipboard.writeText(text)
+          this.copiedText = `Copied ${this.shorturl} to clipboard`;
+        } catch (err) {
+          console.error('Failed to copy!', err)
+        }
      }
      catch(e){
          this.error = e.response.data
      }
     }
+    },
   }
-}
+
 </script>
